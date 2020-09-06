c813103b3baed14f697af220ed23dcf3637cdb37
cheezcharmer <cheezcharmer@yahoo.com>
2020-09-06 15:52:55 +0530

    Refactored the program to use interface

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,35 +1,38 @@
-public class EmpWageBuilder {
-	public static void main(String[] args) {
 
-		EmpWageBuilder a = new EmpWageBuilder();
-
-
-
-		int workingDays = 0;
-
-		int empAbsent = 0;
-		int partTime = 0;
-		int fullTime = 0;
-
-		int totalEmpHours = 0;
-		int empHrs = 0;
-
-		int[] wages = new int[3];
-		String company1 = "Microsoft";
-		String company2 = "Tesla";
-		String company3 = "SpaceX";
-		//Storing all wages in array
-		wages[0] = a.empWage(20,26,workingDays, empAbsent, partTime, fullTime, totalEmpHours, empHrs, 100, company1);
-		wages[1] = a.empWage(30,24, workingDays, empAbsent, partTime, fullTime, totalEmpHours, empHrs, 150, company2);
-		wages[2] = a.empWage(25, 28, workingDays, empAbsent, partTime, fullTime, totalEmpHours, empHrs, 200, company3);
-		//Printing all stored wages
-		System.out.println("Total Wages for month for "+company1+" are "+wages[0]);
-		System.out.println("Total Wages for month for "+company2+" are "+wages[1]);
-		System.out.println("Total Wages for month for "+company3+" are "+wages[2]);
+	interface empWageCalculator {
+		int empWage(int empWagePerHr, int maxWorkingDays, int workingDays, int empAbsent, int partTime, int fullTime, int totalEmpHours, int empHrs, int maxWorkingHrs, String company);
 	}
 
+	public class EmpWageBuilder implements empWageCalculator {
 
-		
+		public static void main(String[] args) {
+
+			EmpWageBuilder a = new EmpWageBuilder();
+
+			int workingDays = 0;
+
+			int empAbsent = 0;
+			int partTime = 0;
+			int fullTime = 0;
+
+			int totalEmpHours = 0;
+			int empHrs = 0;
+
+			int[] wages = new int[3];
+			String company1 = "Microsoft";
+			String company2 = "Tesla";
+			String company3 = "SpaceX";
+			//Storing all wages in array
+			wages[0] = a.empWage(20, 26, workingDays, empAbsent, partTime, fullTime, totalEmpHours, empHrs, 100, company1);
+			wages[1] = a.empWage(30, 24, workingDays, empAbsent, partTime, fullTime, totalEmpHours, empHrs, 150, company2);
+			wages[2] = a.empWage(25, 28, workingDays, empAbsent, partTime, fullTime, totalEmpHours, empHrs, 200, company3);
+			//Printing all stored wages
+			System.out.println("Total Wages for month for " + company1 + " are " + wages[0]);
+			System.out.println("Total Wages for month for " + company2 + " are " + wages[1]);
+			System.out.println("Total Wages for month for " + company3 + " are " + wages[2]);
+		}
+
+
 		public int empWage(int empWagePerHr, int maxWorkingDays, int workingDays, int empAbsent, int partTime, int fullTime, int totalEmpHours, int empHrs, int maxWorkingHrs, String company) {
 			//Calculating work hours until maximum work days and hours are reached
 			while (workingDays < maxWorkingDays && totalEmpHours < maxWorkingHrs) {
@@ -59,12 +62,12 @@ public class EmpWageBuilder {
 				totalEmpHours = totalEmpHours + empHrs;
 			}
 
-			System.out.println("Employee data for "+ company);
+			System.out.println("Employee data for " + company);
 			System.out.println("Employee was Present as Full Time for " + fullTime + " Days");
 
 			System.out.println("Employee was Present as Part Time for " + partTime + " Days");
 
-			System.out.println("Employee was Present as absent for " + empAbsent + " Days");
+			System.out.println("Employee was absent for " + empAbsent + " Days");
 
 			System.out.println("Employee worked for a total of " + totalEmpHours + " Hours in " + workingDays + " Days");
 			// Calculating total wage of employee
@@ -72,6 +75,7 @@ public class EmpWageBuilder {
 			System.out.println("Total Employee Salary for the month is " + empWage);
 
 			System.out.println();
-			return(empWage);
+			return (empWage);
 		}
 	}
+
